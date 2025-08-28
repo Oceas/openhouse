bbc632a5e1652b7f7f07a056715bbfa1899b4b0a
Scott <oceass@gmail.com>
2025-08-28 19:23:07 -0400

    udpated middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'subscription' => \App\Http\Middleware\EnsureUserHasAccess::class,
         ]);
+
+        // Exclude Stripe webhook from CSRF verification
+        $middleware->validateCsrfTokens(except: [
+            'stripe/webhook',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
